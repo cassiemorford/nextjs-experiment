5d80b9605026c264fd27226941b069efbc92cd5a
cassie morford <cassiemorford@gmail.com>
2024-06-04 09:06:25 -0700

    auth setup

---
+++ types/next-auth.d.ts
@@ -0,0 +1,22 @@
+import NextAuth, { DefaultSession } from "next-auth/next";
+import { JWT } from "next-auth/jwt";
+import { Role } from "@prisma/client";
+
+declare module "next-auth" {
+  interface Session {
+    user: { username: string; role: string } & DefaultSession["user"];
+  }
+
+  interface User {
+    id: number;
+    name: string;
+    username: string;
+    role: Role;
+  }
+}
+
+declare module "next-auth/jwt" {
+  interface jwt {
+    role: string;
+  }
+}
